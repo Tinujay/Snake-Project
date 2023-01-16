0b2b0432cab792c0b5b1550bc6535a638c241b6f
run <tinuvazhayil@gmail.com>
2023-01-16 16:05:56 -0500

    completed

---
+++ index.js
@@ -0,0 +1,7 @@
+const snakeNames = require('snake-names')  //pulling in the snake-names module
+
+let namePicked = snakeNames.random() 
+
+console.log('You should use this snake:', namePicked)
+
+//run your first Node.js script
